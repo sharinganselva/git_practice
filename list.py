62dce9d16a44f5ca69e71c11e4b04c8b29df5eca
sharinganselva <selvaprasanna@hotmail.com>
2025-08-30 21:52:52 -0700

    String Manipulation and Lists Questions

--- list.py
+++ list.py
@@ -15,5 +15,71 @@ def find_duplicates(my_list):
     return my_set
 
 
+def two_sum(my_list, target):
+    if not my_list:
+        print("The list is empty")
+
+    my_dict = {}
+    i = 0
+    for n in my_list:
+        if target - n in my_dict:
+            print(f"The two indices are {i}, {my_dict[target-n]}")
+            return True
+        else:
+            my_dict[n] = i
+            i += 1
+
+    print("Cannot find two numbers that sum to the target")
+    return False
+
+
+def move_zeroes(the_list):
+    result = []
+    count = 0
+    for n in the_list:
+        if n == 0:
+            count += 1
+        else:
+            result.append(n)
+
+    while count > 0:
+        result.append(0)
+        count -= 1
+
+    return result
+
+
+def move_zeroes_in_position(the_list):
+    current_index = 0
+    for n in the_list:
+        if n != 0:
+            the_list[current_index] = n
+            current_index += 1
+
+    while current_index < len(the_list):
+        the_list[current_index] = 0
+        current_index += 1
+
+    return the_list
+
+
+def consecutive_ones(the_list):
+    max_count = current_count = 0
+    for n in the_list:
+        if n == 1:
+            current_count += 1
+        else:
+            max_count = max(max_count, current_count)
+            current_count = 0
+    return max_count
+
+
 if __name__ == "__main__":
     print(f"The duplicates are: {find_duplicates([2, 3, -1, -1, 1, 2, 4, 3])}")
+    two_sum([4, 6, 5, 4], 11)
+    print(
+        f"The moving zeroes result = {move_zeroes([0, 0, 0, 0, 0, 0, 3, 0, 0])}")
+    print(
+        f"The moving zeroes in position result = {move_zeroes_in_position([1, 0, 0, 2, 0, 0, 3, 0, 0])}")
+    print(
+        f"Consecutive Ones = {consecutive_ones([1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0])}")
